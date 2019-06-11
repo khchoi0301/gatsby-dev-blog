248bb4466f5cd6bf649a2266e3ed4c093beec988
khchoi0301 <khchoi0301@gmail.com>
2019-06-11 22:54:41 +0900

    modify index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const indexPage = () => {
+const IndexPage = () => {
     return (
         <div>
-
+            <h1>Hello</h1>
+            <h2>
+                This is new blog
+            </h2>
         </div>
     );
 };
 
-export default indexPage;
+export default IndexPage;
